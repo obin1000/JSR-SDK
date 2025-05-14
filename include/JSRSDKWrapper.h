97046fa5ee38efa059b5c3c6ed1caeb4193220e3
Robin Vonk <git@robinvonk.com>
2025-05-14 14:46:27 +0200

    BROKEN: Pushed the limits of native AOT, this is not going to work
    Unhandled exception. JSRDotNETSDK.ExceptionJSRDotNET: Error loading plugins: Operation is not supported on this platform.
     ---> System.PlatformNotSupportedException: Operation is not supported on this platform.
       at System.Reflection.Runtime.Assemblies.RuntimeAssemblyInfo.GetRuntimeAssemblyFromPath(String) + 0x51
       at System.Runtime.Loader.AssemblyLoadContext.LoadFromAssemblyPath(String) + 0x72
       at System.Reflection.Assembly.LoadFrom(String) + 0x170
       at System.ComponentModel.Composition.Hosting.AssemblyCatalog.LoadAssembly(String) + 0xd9
       at System.ComponentModel.Composition.Hosting.AssemblyCatalog..ctor(String, ICompositionElement) + 0x44
       at System.ComponentModel.Composition.Hosting.DirectoryCatalog.CreateAssemblyCatalogGuarded(String) + 0x60
       at System.ComponentModel.Composition.Hosting.DirectoryCatalog.Initialize(String, String) + 0xf8
       at JSRDotNETSDK.JsrPluginHelper.AssembleComponents() + 0xd7
       --- End of inner exception stack trace ---
       at JSRDotNETSDK.JsrPluginHelper.AssembleComponents() + 0x504
       at JSRDotNETSDK.JsrPluginHelper.LoadPlugins(String) + 0x37
       at JSRDotNETSDK.JSRDotNETManager.LoadPlugins(String) + 0x56
       at JSRSDK.NativeExports.LoadPlugins() + 0x3f

--- include/JSRSDKWrapper.h
+++ include/JSRSDKWrapper.h
@@ -5,6 +5,9 @@ extern "C" {
 
 __declspec(dllimport) int __stdcall sdk_add(int a, int b);
 
+__declspec(dllimport) int __stdcall LoadPlugins();
+
+
 #ifdef __cplusplus
 }
 #endif
--- src/JSRSDKWrapper.cs
+++ src/JSRSDKWrapper.cs
@@ -1,10 +1,22 @@
 ﻿namespace JSRSDK;
 
+using JSRDotNETSDK;
 using System.Runtime.InteropServices;
 
 public static class NativeExports
 {
+    private readonly static JSRDotNETManager jsrManager = new();
     // Exposes: int Add(int a, int b);
     [UnmanagedCallersOnly(EntryPoint = "sdk_add")]
     public static int sdk_add(int a, int b) => a + b;
+
+
+    [UnmanagedCallersOnly(EntryPoint = "LoadPlugins")]
+    public static int LoadPlugins()
+    {
+        jsrManager.PluginPath = "C:\\Users\\micro\\Documents\\Projects\\kaminarimedical\\JSR-SDK\\external\\JSRDotNETSDK_1.2.14\\Plugins";
+        jsrManager.LoadPlugins();
+        return 0;
+    }
+
 }
