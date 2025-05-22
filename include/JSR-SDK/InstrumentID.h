4ba31bf0b0cecbc4b51a0e1e780f15bfbc7db5cd
Robin Vonk <git@robinvonk.com>
2025-05-22 14:39:12 +0200

    Generated code docs for mirror classes

--- include/JSR-SDK/InstrumentID.h
+++ include/JSR-SDK/InstrumentID.h
@@ -1,18 +1,49 @@
 #pragma once
 #include <string>
 
+/**
+ * @class InstrumentID
+ * @brief Represents an instrument with identifying details such as model name,
+ * serial number, port, and plugin name.
+ * Mirrors JSRDotNETSDK::IInstrumentIdentity
+ */
 class InstrumentID {
 public:
+  /**
+   * @brief The model name of the instrument.
+   */
   std::string ModelName;
+
+  /**
+   * @brief The serial number of the instrument.
+   */
   std::string SerialNum;
+
+  /**
+   * @brief The port associated with the instrument.
+   */
   std::string Port;
+
+  /**
+   * @brief The name of the plugin associated with the instrument.
+   */
   std::string PluginName;
 
+  /**
+   * @brief Constructs a detailed description of the instrument.
+   * @return A string containing the model name, serial number, port, and plugin
+   * name.
+   */
   std::string GetDescription() const {
     std::string description =
         "Model: " + ModelName + "\n" + "Serial Number: " + SerialNum + "\n" +
         "Port: " + Port + "\n" + "Plugin Name: " + PluginName + "\n";
     return description;
   }
+
+  /**
+   * @brief Converts the instrument details to a string representation.
+   * @return A string containing the instrument's description.
+   */
   std::string ToString() { return GetDescription(); }
 };
--- include/JSR-SDK/PulserReceiverID.h
+++ include/JSR-SDK/PulserReceiverID.h
@@ -3,16 +3,47 @@
 #include "InstrumentID.h"
 #include <string>
 
+/**
+ * @class PulserReceiverID
+ * @brief Represents a Pulser/Receiver with an associated instrument and index.
+ *
+ * This class provides functionality to describe a Pulser/Receiver by combining
+ * its index and the description of the associated instrument.
+ * Mirrors JSRDotNETSDK::PulserReceiverIdentity
+ */
 class PulserReceiverID {
 public:
+  /**
+   * @brief The instrument associated with this Pulser/Receiver.
+   */
   InstrumentID InstrumentId;
+
+  /**
+   * @brief The index of the Pulser/Receiver.
+   */
   int PulserReceiverIndex;
 
+  /**
+   * @brief Constructs a detailed description of the Pulser/Receiver.
+   *
+   * The description includes the Pulser/Receiver index and the description
+   * of the associated instrument.
+   *
+   * @return A string containing the Pulser/Receiver description.
+   */
   std::string GetDescription() const {
     std::string description =
         "Pulser/Receiver: " + std::to_string(PulserReceiverIndex) + "\n" +
         InstrumentId.GetDescription();
     return description;
   }
+
+  /**
+   * @brief Converts the Pulser/Receiver details to a string representation.
+   *
+   * This method is an alias for GetDescription().
+   *
+   * @return A string containing the Pulser/Receiver's description.
+   */
   std::string ToString() { return GetDescription(); }
 };
