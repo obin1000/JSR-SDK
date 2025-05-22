b1526084099b058fdb991d81b4b5fa8e66adcdf5
Robin Vonk <git@robinvonk.com>
2025-05-22 18:26:57 +0200

    Register event handlers on C# side

--- src/JSRSDKWrapper.cpp
+++ src/JSRSDKWrapper.cpp
@@ -12,8 +12,29 @@ ref class JSRSDKWrapper {
 private:
   JSRDotNETManager ^ m_manager;
 
+  void OnStatusChangedEvent(Object ^ sender, EventArgsStatusChange ^ eventData) {
+    System::Console::WriteLine("Status changed: {0}", eventData->ToString());
+  }
+
+  void OnJsrManagerNotify(Object ^ sender, EventArgsManagerNotify ^ eventData) {
+    System::Console::WriteLine("Notified: {0}", eventData->ToString());
+
+  }
+
 public:
-  JSRSDKWrapper() { m_manager = gcnew JSRDotNETManager(""); }
+  JSRSDKWrapper() { 
+      // Create an instance of the JSRDotNETManager
+      m_manager = gcnew JSRDotNETManager("");
+      // Create the C# event receivers
+      // TODO: Only register these when the C++ side has a handler for them
+      m_manager->StatusChangeEventHandler +=
+          gcnew System::EventHandler<EventArgsStatusChange ^>(
+              this, &JSRSDKWrapper::OnStatusChangedEvent);
+      m_manager->NotifyEventHandler +=
+          gcnew System::EventHandler<EventArgsManagerNotify ^>(
+              this, &JSRSDKWrapper::OnJsrManagerNotify);
+
+  }
 
   property JSRDotNETManager ^
       dotNETManager { JSRDotNETManager ^ get() { return m_manager; } }
